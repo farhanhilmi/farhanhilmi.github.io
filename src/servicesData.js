d173a948ed2e80a4261e6282d88603a59a93885c
Farhan Hilmi <farhanhilmi077@gmail.com>
2021-08-01 23:03:28 +0700

    fix

--- src/servicesData.js
+++ src/servicesData.js
@@ -8,18 +8,21 @@ const servicesData = [
     title: 'Front-End',
     img: logoFrontEnd,
     text: 'I develop front-end with mobile first principle, to ensure responsiveness across all devices.',
+    margin: 70,
   },
   {
     id: 2,
     title: 'Back-End',
     img: logoBackEnd,
     text: 'Every app needs someone who is responsible for your data flow and data organization. I ensuring everything on the server-side actually works.',
+    margin: 70,
   },
   {
     id: 3,
     title: 'Full Stack Web',
     img: logoUX,
     text: 'Develop a fully fledged website platform from the client side and server side',
+    margin: 0,
   },
   // {
   //   id: 3,
